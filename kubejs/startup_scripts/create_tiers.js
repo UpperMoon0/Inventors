51ecad080ecb30ed8e6172415cf53092bfc60cca
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-03-16 21:36:02 +0700

    feat: Implement custom world generation for various ores, define Create tiers, and configure JEI recipe category sorting.

---
+++ kubejs/startup_scripts/create_tiers.js
@@ -0,0 +1,5 @@
+// Shaft, cogwheel
+CreateTiers.registerTier('crude', 1, 128, 512, 0x6B4E2C, 0x6B4E2C, 'Crude');
+CreateTiers.registerTier('basic', 2, 256, 2048, 0x936C3D, 0xBCBCBC, 'Basic');
+CreateTiers.registerTier('refined', 3, 512, 8192, 0xA995BB, 0x49EAD6, 'Refined');
+
