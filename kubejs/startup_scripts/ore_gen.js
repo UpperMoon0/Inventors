c34f43016363ad7501e4a6941def03099069fab1
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-03-16 19:37:03 +0700

    feat: Add new mods and implement custom world generation for various ores including tin, silver, lead, nickel, and uranium.

--- kubejs/startup_scripts/ore_gen.js
+++ kubejs/startup_scripts/ore_gen.js
@@ -1,6 +1,6 @@
 // Disable crusty_chunks ore generation
 
-onEvent('worldgen.remove', event => {
+WorldgenEvents.remove(event => {
     event.removeOres(ores => {
         ores.blocks = [
             'crusty_chunks:lead_ore',
@@ -12,71 +12,5 @@ onEvent('worldgen.remove', event => {
             'crusty_chunks:uranium_ore',
             'crusty_chunks:deepslate_uranium_ore'
         ]
-    })
-})
-
-// Add ftbmaterials tin ore generation to overworld
-onEvent('worldgen.add', event => {
-    event.addOre(ore => {
-        ore.id = 'kubejs:tin_ore'
-        ore.biomes = '#minecraft:is_overworld'
-        ore.addTarget('minecraft:stone', 'ftbmaterials:tin_stone_ore')
-        ore.addTarget('minecraft:deepslate', 'ftbmaterials:tin_deepslate_ore')
-        ore.size(9)
-        ore.count(20)
-        ore.squaredRange(-64, 64)
-        ore.triangleHeight(0, 64)
-        ore.noop()
-    })
-})
-
-// Add ftbmaterials nether ore generation
-onEvent('worldgen.add', event => {
-    // Silver ore in Nether
-    event.addOre(ore => {
-        ore.id = 'kubejs:silver_nether_ore'
-        ore.biomes = '#minecraft:is_nether'
-        ore.addTarget('minecraft:netherrack', 'ftbmaterials:silver_nether_ore')
-        ore.size(9)
-        ore.count(15)
-        ore.squaredRange(0, 128)
-        ore.uniformHeight(0, 128)
-        ore.noop()
-    })
-
-    // Lead ore in Nether
-    event.addOre(ore => {
-        ore.id = 'kubejs:lead_nether_ore'
-        ore.biomes = '#minecraft:is_nether'
-        ore.addTarget('minecraft:netherrack', 'ftbmaterials:lead_nether_ore')
-        ore.size(9)
-        ore.count(15)
-        ore.squaredRange(0, 128)
-        ore.uniformHeight(0, 128)
-        ore.noop()
-    })
-
-    // Nickel ore in Nether
-    event.addOre(ore => {
-        ore.id = 'kubejs:nickel_nether_ore'
-        ore.biomes = '#minecraft:is_nether'
-        ore.addTarget('minecraft:netherrack', 'ftbmaterials:nickel_nether_ore')
-        ore.size(9)
-        ore.count(15)
-        ore.squaredRange(0, 128)
-        ore.uniformHeight(0, 128)
-        ore.noop()
-    })
-
-    // Uranium ore in Nether
-    event.addOre(ore => {
-        ore.id = 'kubejs:uranium_nether_ore'
-        ore.biomes = '#minecraft:is_nether'
-        ore.addTarget('minecraft:netherrack', 'ftbmaterials:uranium_nether_ore')
-        ore.size(7)
-        ore.count(10)
-        ore.squaredRange(0, 128)
-        ore.uniformHeight(0, 128)
-        ore.noop()
     })
 })
