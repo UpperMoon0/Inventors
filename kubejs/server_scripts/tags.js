09ce7b472fd71ac34bb8334eba73c6cdc5627c66
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-08-29 01:06:13 +0700

    feat: classify Aquaculture animals for Firstworks drops

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -56,3 +56,47 @@ ServerEvents.tags('item', event => {
     event.add('kubejs:large_cogwheel', 'createtiers:large_cogwheel_basic')
     event.add('kubejs:large_cogwheel', 'createtiers:large_cogwheel_refined')
 })
+
+// Pack-owned extensions to Firstworks' generic animal-material rules.
+// Aquaculture is the only installed mod that registers living animal entities.
+// Its fish and turtles are vertebrates; jellyfish are explicitly excluded.
+ServerEvents.tags('entity_type', event => {
+    event.add('firstworks:drops_bones', [
+        'aquaculture:arapaima',
+        'aquaculture:arrau_turtle',
+        'aquaculture:atlantic_cod',
+        'aquaculture:atlantic_halibut',
+        'aquaculture:atlantic_herring',
+        'aquaculture:bayad',
+        'aquaculture:blackfish',
+        'aquaculture:bluegill',
+        'aquaculture:boulti',
+        'aquaculture:box_turtle',
+        'aquaculture:brown_shrooma',
+        'aquaculture:brown_trout',
+        'aquaculture:capitaine',
+        'aquaculture:carp',
+        'aquaculture:catfish',
+        'aquaculture:gar',
+        'aquaculture:minnow',
+        'aquaculture:muskellunge',
+        'aquaculture:pacific_halibut',
+        'aquaculture:perch',
+        'aquaculture:pink_salmon',
+        'aquaculture:piranha',
+        'aquaculture:pollock',
+        'aquaculture:rainbow_trout',
+        'aquaculture:red_grouper',
+        'aquaculture:red_shrooma',
+        'aquaculture:smallmouth_bass',
+        'aquaculture:starshell_turtle',
+        'aquaculture:synodontis',
+        'aquaculture:tambaqui',
+        'aquaculture:tuna'
+    ])
+
+    event.add('firstworks:no_bone_drops', 'aquaculture:jellyfish')
+
+    // No installed mod adds a leather-bearing animal. Aquaculture fish and
+    // shelled turtles intentionally do not receive raw-hide entries.
+})
